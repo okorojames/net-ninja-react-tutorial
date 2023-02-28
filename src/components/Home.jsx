cf4e6fd9e9ca25110480a0e049f3b85a87d79728
okorojames <okorojameschizaram@gmail.com>
2023-02-28 16:52:43 +0100

    still
    learning reactjs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
   {title: 'Welcome to party', body: 'lorem ipsum...', author: 'yoshi', id: 2},
   {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
-])
+  ])
   return (
     <div className='Home'>
       {blogs.map((blog) => (
