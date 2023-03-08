cb78d8fc87805aacbd13f34fe7d62448fa01a2a8
okorojames <okorojameschizaram@gmail.com>
2023-03-08 13:58:43 +0100

    created a db file to store my blogs data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import BlogList from './BlogList'
 
 const Home = () => {
@@ -9,14 +9,23 @@ const Home = () => {
   {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
   ])
   //
+  const [name, setName] = useState("mario")
+  // 
   const handleDel = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id)
     setBlogs(newBlogs)
   }
   // 
+  useEffect(() => {
+    console.log('useeffect ran')
+    console.log(name)
+  }, [name])
+  // 
   return (
     <div className='Home'>
-      <BlogList blogs={blogs} title='All Blogs' handleDel={handleDel}/>
+      <BlogList blogs={blogs} title='All Blogs' handleDel={handleDel} />
+      <button onClick={() => setName("Luigi")}>change name</button>
+      <p>{name}</p>
     </div>
   )
 }
