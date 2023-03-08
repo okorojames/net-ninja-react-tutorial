04a7b5f9894f5233b39fe4429c6b1e72f97a40d8
okorojames <okorojameschizaram@gmail.com>
2023-03-08 11:23:31 +0100

    Function as props still learning how to pass function as props.

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const BlogList = ({blogs, title}) => {
+const BlogList = ({blogs, title, handleDel}) => {
   return (
     <div>
       <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
-          <span>Written by {blog.author}</span>
+          <p>Written by {blog.author}</p>
+          <button onClick={()=> handleDel(blog.id)}>Delete Blog</button>
         </div>
       ))}
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,10 +8,15 @@ const Home = () => {
   {title: 'Welcome to party', body: 'lorem ipsum...', author: 'yoshi', id: 2},
   {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
   ])
+  //
+  const handleDel = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id)
+    setBlogs(newBlogs)
+  }
+  // 
   return (
     <div className='Home'>
-      <BlogList blogs={blogs} title='All Blogs' />
-      <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Mario's Blogs" />
+      <BlogList blogs={blogs} title='All Blogs' handleDel={handleDel}/>
     </div>
   )
 }
