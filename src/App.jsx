031edb73f6c37a66f38d3de2afef87d4c911c9c5
okorojames <okorojameschizaram@gmail.com>
2023-04-19 14:14:00 +0100

    starting afresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home/>
-      </div>
-    </div>
-  )
+  return <div className="App"></div>;
 }
 
-export default App
+export default App;
